e22a27c6ef8aeef04624f13731d8167194998c4e
vivek <pattanayak.vivek@gmail.com>
2021-08-02 12:43:42 +0530

    few optimisations

--- public/js/script.js
+++ public/js/script.js
@@ -1,21 +1,35 @@
 async function comparefunction(){
-    let url1=document.getElementById('info1').value; 
-    url1="https://codeforces.com/api/user.info?handles=" + url1;
+    let url1="https://codeforces.com/api/user.info?handles=" + document.getElementById('info1').value;
     const res1= await fetch(url1);
+    let url2="https://codeforces.com/api/user.info?handles=" + document.getElementById('info2').value;
+    const res2= await fetch(url2);
+    let a1,a2,user1,user2,ratingurl1,rating_exists1,ratingurl2,rating_exists2;
+    if(res1.ok){
+        a1= await res1.json();
+        user1 = a1.result[0];
+        ratingurl1="https://codeforces.com/api/user.rating?handle="+user1.handle;
+        rating_exists1= await fetch(ratingurl1);
+    }
+    if(res2.ok){
+        a2= await res2.json();
+        user2 = a2.result[0];
+        ratingurl2="https://codeforces.com/api/user.rating?handle="+user2.handle;
+        rating_exists2= await fetch(ratingurl2);
+    }
     if(res1.ok)
     {
-        const a1= await res1.json();
-        const user1 = a1.result[0];
+        let u_col=colorcheck(user1.rank);
         document.getElementById('tab_1').style.display='block';
         document.getElementById('user1dp').src=user1.titlePhoto;
         document.getElementById('user1dp').style.height='200px';
         document.getElementById('user1dp').style.width='200px';
+        document.getElementById('user1dp').style.border='4px outset '+u_col;
         document.getElementById('handle1').innerHTML=user1.handle;
         if(user1.rank!='legendary grandmaster'){
-            document.getElementById('handle1').style.color=colorcheck(user1.rank);
+            document.getElementById('handle1').style.color=u_col;
         }
         else{
-            document.getElementById('handle1').style.color=colorcheck(user1.rank);
+            document.getElementById('handle1').style.color=u_col;
             let user_str=user1.handle;
             let str="<span style='color:black;'>"+user_str.charAt(0)+"</span><span>";
             for (var i = 1; i < user_str.length; i++) {
@@ -25,23 +39,21 @@ async function comparefunction(){
             document.getElementById("handle1").innerHTML = str;
         } 
         document.getElementById('name1').innerHTML=user1.firstName+' '+user1.lastName;
-        document.getElementById('rating1').innerHTML=' <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user1.rank)+';">' + user1.rating + '</span>' + '  ( </span> <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user1.rank)+';">' + user1.rank + '</span>)' ;
-        document.getElementById('maxrating1').innerHTML=' <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user1.maxRank)+';">' + user1.maxRating + '</span>' + '  ( </span> <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user1.maxRank)+';">' + user1.maxRank + '</span>)' ;
+        document.getElementById('rating1').innerHTML=' <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+u_col+';">' + user1.rating + '</span>' + '  ( </span> <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user1.rank)+';">' + user1.rank + '</span>)' ;
+        document.getElementById('maxrating1').innerHTML=' <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+u_col+';">' + user1.maxRating + '</span>' + '  ( </span> <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user1.maxRank)+';">' + user1.maxRank + '</span>)' ;
         //assume ok because he has an entity already existing
 
         //start of rating graph 
-
-        let ratingurl="https://codeforces.com/api/user.rating?handle="+user1.handle;
-        const rating_exists= await fetch(ratingurl);
-        if(rating_exists.ok){
-            const obj1= await rating_exists.json();
+        if(rating_exists1.ok){
+            const obj1= await rating_exists1.json();
             var Xval=[];
             var Yval=[];
             document.getElementById("contests_appeared1").innerHTML=obj1.result.length;
             let bestrank='1000000',worstrank=0,bestrankroundname=' NA ',worstrankroundname=' NA ',maxdplus=0,maxdminus=0,bestroundname=' NA ',worstroundname=' NA ';
+            let x1=0;
             for(var i = 0; i < obj1.result.length; i++) {
+                x1=x1+1;
                 let t_obj=obj1.result[i];
-                let x1=t_obj.contestId; 
                 let y1=t_obj.newRating;
                 Xval.push(x1);Yval.push(y1);
                 if(t_obj.rank<bestrank){
@@ -151,23 +163,20 @@ async function comparefunction(){
         document.getElementById('maxrating1').innerHTML='  ';
     }
 
-    let url2=document.getElementById('info2').value; 
-    url2="https://codeforces.com/api/user.info?handles=" + url2;
-    const res2= await fetch(url2);
     if(res2.ok)
     {
-        const a2= await res2.json();
-        const user2 = a2.result[0];
+        let u_col=colorcheck(user2.rank);
         document.getElementById('tab_2').style.display='block';
         document.getElementById('user2dp').src=user2.titlePhoto;
         document.getElementById('user2dp').style.height='200px';
         document.getElementById('user2dp').style.width='200px';
+        document.getElementById('user2dp').style.border='4px outset '+u_col;
         document.getElementById('handle2').innerHTML=user2.handle;
         if(user2.rank!='legendary grandmaster'){
-            document.getElementById('handle2').style.color=colorcheck(user2.rank);
+            document.getElementById('handle2').style.color=u_col;
         }
         else{
-            document.getElementById('handle2').style.color=colorcheck(user2.rank);
+            document.getElementById('handle2').style.color=u_col;
             let user_str=user2.handle;
             let str="<span style='color:black;'>"+user_str.charAt(0)+"</span><span>";
             for (var i = 1; i < user_str.length; i++) {
@@ -177,23 +186,19 @@ async function comparefunction(){
             document.getElementById("handle2").innerHTML = str;
         } 
         document.getElementById('name2').innerHTML=user2.firstName+' '+user2.lastName;
-        document.getElementById('rating2').innerHTML='<span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user2.rank)+';">' + user2.rating + '</span>' + '  ( </span> <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user2.rank)+';">' + user2.rank + '</span>)' ;
-        document.getElementById('maxrating2').innerHTML='<span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user2.maxRank)+';">' + user2.maxRating + '</span>' + '  ( </span> <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user2.maxRank)+';">' + user2.maxRank + '</span>)' ;
+        document.getElementById('rating2').innerHTML='<span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+u_col+';">' + user2.rating + '</span>' + '  ( </span> <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user2.rank)+';">' + user2.rank + '</span>)' ;
+        document.getElementById('maxrating2').innerHTML='<span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+u_col+';">' + user2.maxRating + '</span>' + '  ( </span> <span style="font-size:13px;font-family:verdana;font-weight:bold;color:'+colorcheck(user2.maxRank)+';">' + user2.maxRank + '</span>)' ;
         //assume ok because he has an entity already existing
-        let ratingurl="https://codeforces.com/api/user.rating?handle="+user2.handle;
-
-        //start of rating graph 
-
-        const rating_exists= await fetch(ratingurl);
-        if(rating_exists.ok){
-            const obj1= await rating_exists.json();
+        if(rating_exists2.ok){
+            const obj1= await rating_exists2.json();
             document.getElementById("contests_appeared2").innerHTML=obj1.result.length;
             var Xval=[];
             var Yval=[];
             let bestrank='1000000',worstrank=0,bestrankroundname=' NA ',worstrankroundname=' NA ',maxdplus=0,maxdminus=0,bestroundname=' NA ',worstroundname=' NA ';
+            let x1=0;
             for(var i = 0; i < obj1.result.length; i++) {
                 let t_obj=obj1.result[i];
-                let x1=t_obj.contestId; 
+                x1=x1+1;
                 let y1=t_obj.newRating;
                 Xval.push(x1);Yval.push(y1);
                 if(t_obj.rank<bestrank){
